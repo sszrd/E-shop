11d42cb045cc15c789de8ef5f6724c7af03601ba
Wang Zijie <1327000313@qq.com>
2022-02-07 16:14:44 +0800

    update swiper

--- src/components/swiper/index.tsx
+++ src/components/swiper/index.tsx
@@ -16,16 +16,19 @@ const Swiper: FC<IProps> = (props: IProps): ReactElement => {
     const [shiftX, setShiftX] = useState<number>(0);  //总偏移量
     const [prevX, setPrevX] = useState<number>(0);  //上一次总偏移量
     const [startX, setStartX] = useState<number>(0);  //开始触摸时手指x坐标
-
     const autoPlay = useCallback(() => {
         timer.current = setInterval(() => {
             setCurIndex(curIndex => {
                 if (!picList.current || !picNav.current) {
                     return curIndex;
                 }
+
                 if (curIndex === picList.current.children.length - 1) {
-                    return picNav.current.children.length - 1;
+                    picList.current.style.transition = "none";
+                    picList.current.style.transform = `translate3d(${-(picNav.current.children.length - 1) * picList.current.parentElement!.getBoundingClientRect().width}px)`;
+                    return picNav.current.children.length;
                 }
+                picList.current.style.transition = "1.5s";
                 return curIndex + 1;
             })
         }, 3000)
@@ -52,33 +55,12 @@ const Swiper: FC<IProps> = (props: IProps): ReactElement => {
 
     //根据索引计算总偏移量
     useEffect(() => {
-        if (!picList.current || !picNav.current) {
-            return;
-        }
         setShiftX(shiftX => {
-            if (!picList.current || !picNav.current) {
+            if (!picList.current) {
                 return shiftX;
             }
-            if (curIndex === picList.current.children.length - 1) {
-                picList.current.style.transition = "none";
-            } else {
-                picList.current.style.transition = "1.5s";
-            }
-            return -curIndex * picList.current.parentElement!.clientWidth;
+            return -curIndex * picList.current.parentElement!.getBoundingClientRect().width;
         })
-    }, [curIndex])
-
-    //使轮播图对应的导航栏高亮
-    useEffect(() => {
-        if (!picNav.current) {
-            return;
-        }
-        Array.from(picNav.current.children).forEach((item) => {
-            item.classList.remove("active");
-        })
-        Array.from(picNav.current.children)[
-            curIndex % Array.from(picNav.current.children).length
-        ].classList.add("active");
     }, [curIndex])
 
     const handleStart = (e: React.TouchEvent<HTMLUListElement>): any => {
@@ -91,12 +73,11 @@ const Swiper: FC<IProps> = (props: IProps): ReactElement => {
         //滑动时禁止过渡
         picList.current.style.transition = "none";
         setStartX(e.changedTouches[0].pageX);
-        //边界处理
         //当幻灯片到了第一组第一张将其移动到第二组第一张，当幻灯片到了第二组最后一张将其移动到第一组最后一张
         if (curIndex === 0) {
             setCurIndex(picNav.current.children.length);
         } else if (curIndex === picList.current.children.length - 1) {
-            setCurIndex(picNav.current.children.length - 1)
+            setCurIndex(picNav.current.children.length - 1);
         }
         setPrevX(shiftX);
     }
@@ -114,35 +95,22 @@ const Swiper: FC<IProps> = (props: IProps): ReactElement => {
         const curX = e.changedTouches[0].pageX;
         const differ = curX - startX;
         //当滑动距离超过0.3屏幕宽度时切换上/下一张
-        if (Math.abs(differ) > 0.3 * picList.current.parentElement!.clientWidth) {
+        picList.current.style.transition = "1.5s";
+        if (Math.abs(differ) > 0.3 * picList.current.parentElement!.getBoundingClientRect().width) {
             setCurIndex(curIndex => curIndex - differ / Math.abs(differ));
         } else {
-            setShiftX(-curIndex * picList.current.parentElement!.clientWidth);
+            setShiftX(-curIndex * picList.current.parentElement!.getBoundingClientRect().width);
         }
-        picList.current.style.transition = "0.5s";
         autoPlay();
-    }
-
-    const getNav = () => {
-        if (props.dataSource.length > 1) {
-            return (
-                <nav className="picNav" ref={picNav}>
-                    {
-                        props.dataSource.map((item, index) => {
-                            return <span key={index} className={index === 0 ? "active" : ""}></span>
-                        })
-                    }
-                </nav>
-            )
-        }
-        return "";
     }
 
     return (
         <div className="tab ">
             <ul className="picList"
                 ref={picList}
-                style={{ transform: `translate3d(${shiftX}px,0,0)`, }}
+                style={{
+                    transform: `translate3d(${shiftX}px,0,0)`,
+                }}
                 onTouchStart={handleStart}
                 onTouchMove={handleMove}
                 onTouchEnd={handleEnd}
@@ -159,8 +127,18 @@ const Swiper: FC<IProps> = (props: IProps): ReactElement => {
                     })
                 }
             </ul>
-            {getNav()}
+            <nav className="picNav" ref={picNav}>
+                {
+                    props.dataSource.map((item, index) => {
+                        return <span key={index} className={picNav.current ?
+                            index === curIndex % Array.from(picNav.current.children).length ? "active" : ""
+                            : ""
+                        }></span>
+                    })
+                }
+            </nav>
         </div >
     );
 }
+
 export default Swiper;
